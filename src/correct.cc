0f1ad9831d019f8533a9a1eca99379f62056b232
mam-mih-val <mam.mih.val@gmail.com>
2020-07-24 17:50:55 +0300

    Recentring+twist+rescale on un

--- src/correct.cc
+++ src/correct.cc
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
   Qn::QvectorTracksConfig un_vector("u",
                                   {vtx_tracks, "phi"}, {"Ones"},
                                   {pt_axis, rapidity_axis});
-  un_vector.SetCorrectionSteps(true, false, false);
+  un_vector.SetCorrectionSteps(true, true, true);
   un_vector.AddCut( {AnalysisTree::Variable("mdc_vtx_tracks","geant_pid")},
                  [](double pid) { return abs(pid - 14.0) < 0.1; } );
   un_vector.SetType(Qn::Stats::Weights::OBSERVABLE);
@@ -79,7 +79,7 @@ int main(int argc, char **argv) {
   Qn::QvectorTracksConfig qn_mdc("M",
                                     {vtx_tracks, "phi"}, {"Ones"},
                                     {rapidity_axis});
-  qn_mdc.SetCorrectionSteps(true, false, false);
+  qn_mdc.SetCorrectionSteps(true, true, true);
   qn_mdc.AddCut( {AnalysisTree::Variable("mdc_vtx_tracks","geant_pid")},
                     [](double pid) { return abs(pid - 14.0) < 0.1; } );
   qn_mdc.SetType(Qn::Stats::Weights::REFERENCE);
@@ -91,10 +91,11 @@ int main(int argc, char **argv) {
   Qn::CorrectTaskManager task_manager({file_list}, {"hades_analysis_tree"});
   auto* task = new Qn::CorrectionTask(global_config);
 
-  task->AddQAHistogram("u", {{vtx_tracks + "_rapidity", 100, -0.75+beam_y, 0.75+beam_y},
-                           {vtx_tracks + "_pT", 100, 0.0, 2.0},});
+  task->AddQAHistogram("u", {{vtx_tracks + "_rapidity", 200, -0.75+beam_y, 0.75+beam_y},
+                           {vtx_tracks + "_pT", 200, 0.0, 2.0}});
 
-  task->AddQAHistogram("u", {{vtx_tracks + "_pT", 200, 0.0, 5.0}});
+  task->AddQAHistogram("u", {{vtx_tracks + "_rapidity", 100, -0.75+beam_y, 0.75+beam_y},
+                           {vtx_tracks + "_phi", 315, -3.15, 3.15}});
 
   task_manager.AddBranchCut(AnalysisTree::GetHadesTrackCuts(vtx_tracks));
   task_manager.AddBranchCut(AnalysisTree::GetHadesEventCuts(event_header));
@@ -104,7 +105,7 @@ int main(int argc, char **argv) {
   task_manager.AddTask(task);
   task_manager.Init();
   auto start = std::chrono::system_clock::now();
-  task_manager.Run(-1);
+  task_manager.Run(1000);
   task_manager.Finish();
   auto end = std::chrono::system_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
