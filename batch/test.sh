4f10a1d9b38c7ae4ce657f2ec81f9e9e860f2d80
mam-mih-val <mam.mih.val@gmail.com>
2020-08-05 15:56:17 +0300

    bug fixed

--- batch/test.sh
+++ batch/test.sh
@@ -2,17 +2,21 @@
 
 build_dir=/home/mikhail/hades_flow/cmake-build-debug
 
+rm correction_in.root
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/mikhail/AnalysisTree/install/lib
+
 echo "executing $build_dir/correct ${1}"
 $build_dir/correct ${1}
 mv correction_out.root correction_in.root
 
-#echo "executing $build_dir/correct ${1}"
-#$build_dir/correct ${1}
-#mv correction_out.root correction_in.root
-#
-#echo "executing $build_dir/correct ${1} "
-#$build_dir/correct ${1}
-#mv correction_out.root correction_in.root
+echo "executing $build_dir/correct ${1}"
+$build_dir/correct ${1}
+mv correction_out.root correction_in.root
+
+echo "executing $build_dir/correct ${1} "
+$build_dir/correct ${1}
+mv correction_out.root correction_in.root
 
 echo "executing $build_dir/correlate correction_out.root"
 $build_dir/correlate correction_in.root
