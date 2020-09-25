f79760c4e10aff695bfde5113b0d26561b59fadd
mam-mih-val <mam.mih.val@gmail.com>
2020-09-25 16:43:30 +0300

    Batch macro changed according to new interface

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -27,16 +27,16 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/nyx/hades/user/mmamaev/install/A
 echo "loading " $ownroot
 source $ownroot
 
-echo "executing $build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root"
-$build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root
+echo "executing $build_dir/correct -i list.txt"
+$build_dir/correct -i list.txt
 mv correction_out.root correction_in.root
 
-echo "executing $build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root"
-$build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root
+echo "executing $build_dir/correct -i list.txt"
+$build_dir/correct -i list.txt
 mv correction_out.root correction_in.root
 
-echo "executing $build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root"
-$build_dir/correct list.txt /lustre/nyx/hades/user/mmamaev/hades_flow/src/param/efficiency.root
+echo "executing $build_dir/correct -i list.txt"
+$build_dir/correct -i list.txt
 mv correction_out.root correction_in.root
 
 echo "executing $build_dir/correlate correction_out.root"
